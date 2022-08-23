c415db359c9750ad3b4008d59c1117e570f9332c
Manan1153 <63907151+Manan1153@users.noreply.github.com>
2022-08-22 22:54:34 -0400

    Add files via upload

---
+++ Workshop 3/Set.h
@@ -0,0 +1,15 @@
+#ifndef SDDS_SET_H
+#define SDDS_SET_H
+
+#include <iostream>
+#include <cmath>
+#include "Pair.h"
+#include "Collection.h"
+#include <string>
+
+namespace sdds
+{
+	template<class T>
+
+}
+#endif // !SDDS_SET_H
